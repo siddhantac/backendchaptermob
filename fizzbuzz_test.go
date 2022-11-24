6433e768d817d1b38a28afc4b5131656b0a701cc
Josiah Lee <josiah.lee@foodpanda.com>
2022-11-24 14:25:35 +0800

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:fizzbuzz_test.go

--- fizzbuzz_test.go
+++ fizzbuzz_test.go
@@ -1,8 +1,40 @@
 package fizzbuzz
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
-func TestFizzbuzz(t *testing.T) {
-	// if a number is divisible by 3, return fizz
-	calculateFizzbuzz(3)
+func Test_calculateFizzbuzz(t *testing.T) {
+	tests := []struct {
+		name string
+		num  int
+		want string
+	}{
+		{
+			name: "Number divisible by 3 gives fizz",
+			num:  3,
+			want: "fizz",
+		},
+		{
+			name: "Number divisible by 5 gives buzz",
+			num:  5,
+			want: "buzz",
+		},
+		{
+			name: "Number divisible by 3 AND 5 gives fizzbuzz",
+			num:  15,
+			want: "fizzbuzz",
+		},
+		{
+			name: "Numbers not divisible by 3 OR 5 gives empty string",
+			num:  2,
+			want: "",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, calculateFizzbuzz(tt.num), "calculateFizzbuzz(%v)", tt.num)
+		})
+	}
 }
